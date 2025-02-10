7d88a4665563ea8a283cd7adcfb5f62221ae53e8
wns0901 <wnswns012@gmail.com>
2025-02-10 14:54:21 +0900

    Feat(#8): 채팅 기능 완료

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
+import { Provider } from "react-redux";
+import { myStore } from "./containers/store";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ChatComponent from "./components/ChatComponent";
 import LoginPage from "./domains/LoginPage/LoginPage";
+import ChatComponent from "./components/chat/ChatComponent";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginContextProvider from "./contexts/LoginContextProvider";
+import SampleIndex from "./SampleIndex";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <LoginContextProvider>
-      <Routes>
-        <Route path="/" element={<ChatComponent />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </LoginContextProvider>
+    <Provider store={myStore}>
+      <LoginContextProvider>
+        <ChatComponent />
+        <Routes>
+          <Route path="/" element={<SampleIndex />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </LoginContextProvider>
+    </Provider>
   </BrowserRouter>
 );

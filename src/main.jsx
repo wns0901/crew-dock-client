9e684d7d2db05ef70140e828e8c1cdb1a000f772
sihooh <khy2001smy@gmail.com>
2025-02-13 00:53:10 +0900

    Feat(#5): main에서 issueTable 제거

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ChatComponent from "./components/ChatComponent";
 import LoginPage from "./domains/LoginPage/LoginPage";
 import LoginContextProvider from "./contexts/LoginContextProvider";
-import IssueTable from "./domains/ProjectPage/components/IssueTable";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -11,7 +10,6 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<ChatComponent />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/projectIssue" element={<IssueTable/>}/>
       </Routes>
     </LoginContextProvider>
   </BrowserRouter>

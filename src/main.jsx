86bafe1c2a040c7bdcaba763025b0725f33e1283
wns0901 <wnswns012@gmail.com>
2025-02-14 15:25:47 +0900

    Feat(): import 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { Provider } from "react-redux";
+import { myStore } from "./containers/store"
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ChatComponent from "./components/chat/ChatComponent";
